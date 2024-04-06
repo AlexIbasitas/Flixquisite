8a82d994e7cdaa28550cd511261e0f14ab71b9a6
alexi <aibasita@uci.edu>
2024-04-05 22:30:08 -0700

    add unique identifier via uuid and override __str__ method to return movie title

--- flixquisite_site/core/models.py
+++ flixquisite_site/core/models.py
@@ -1,27 +1,32 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 class Movie(models.Model):
-    GENRES = [
-        ('action', 'Action'),
-        ('adventure', 'Adventure'),
-        ('horror', 'Horror'),
-        ('romance', 'Romance'),
-        ('science_fiction', 'Science Fiction'),
-        ('thriller', 'Thriller'),
-        ('fantasy', 'Fantasy'),
-        ('animation', 'Animation'),
-        ('documentary', 'Documentary'),
-        ('mystery', 'Mystery')
-    ]
+    def __init__(self):
+        GENRES = [
+            ('action', 'Action'),
+            ('adventure', 'Adventure'),
+            ('horror', 'Horror'),
+            ('romance', 'Romance'),
+            ('science_fiction', 'Science Fiction'),
+            ('thriller', 'Thriller'),
+            ('fantasy', 'Fantasy'),
+            ('animation', 'Animation'),
+            ('documentary', 'Documentary'),
+            ('mystery', 'Mystery')
+        ]
 
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    date = models.DateField()
-    genre = models.CharField(max_length=50, choices=GENRES)
+        id = models.UUIDField(default=uuid.uuid4)
 
-    length = models.PositiveBigIntegerField()
+        title = models.CharField(max_length=255)
+        description = models.TextField()
+        date = models.DateField()
+        genre = models.CharField(max_length=50, choices=GENRES)
+        length = models.PositiveBigIntegerField()
 
-    image_carousel_card = models.ImageField(upload_to='movie_images/')
-    image_feature_cover = models.FileField(upload_to='movie_videos/')
-    
+        image_carousel_card = models.ImageField(upload_to='movie_images/')
+        image_feature_cover = models.FileField(upload_to='movie_images/')
+        playback = models.FileField(upload_to='movie_videos/')
+
+    def __str__(self):
+        return self.title
