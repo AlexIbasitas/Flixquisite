5f0120ad68c9c0d96b315bf2bcc426d829abd98f
alexi <aibasita@uci.edu>
2024-04-05 21:19:46 -0700

    User attributes from POST collected

--- flixquisite_site/core/views.py
+++ flixquisite_site/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.models import auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,4 +13,28 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        # Gather signup attributes sent from User in the signup form
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+
+        # Check user has correctly inputted both passswords
+        if password != repeat_password:
+            # send message back to signup.html
+            messages.info(request, 'Passwords do not match, please try again.')
+            # send user back to the signup page
+            return redirect('signup')
+
+        # Check if user has already signed up with this email if the passwords match
+        # Check DB by filtering User table and seeing if the email received from the POST method matches any email in the User table
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email has already been used.')
+            # send user back to the signup page
+            return redirect('signup')
+
+        return
+    
     return render(request, 'signup.html')
+
