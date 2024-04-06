da8b4ce5360e886633a582255399cc1083fa677a
alexi <aibasita@uci.edu>
2024-04-05 21:26:45 -0700

    Checked for duplicate usernames. Added create_user functionality and redirection to homepage for authenticated users

--- flixquisite_site/core/views.py
+++ flixquisite_site/core/views.py
@@ -19,7 +19,7 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
         repeat_password = request.POST['repeat_password']
-        
+
         # Check user has correctly inputted both passswords
         if password != repeat_password:
             # send message back to signup.html
@@ -27,16 +27,22 @@ def signup(request):
             # send user back to the signup page
             return redirect('signup')
 
-        # Check if user has already signed up with this email if the passwords match
         # Check DB by filtering User table and seeing if the email received from the POST method matches any email in the User table
         if User.objects.filter(email=email).exists():
-            messages.info(request, 'Email has already been used.')
+            messages.info(request, 'Email has already been used, please try again.')
             # send user back to the signup page
             return redirect('signup')
-
-
-
-        return
+        # Check that username has not already been taken
+        elif User.objects.filter(username=username).exists():
+            messages.info(request, 'Username has already been taken, please try again.')
+            # send user back to the signup page
+            return redirect('signup')
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            auth_user = auth.authenticate(username=username, password=password)
+            auth.login(request, auth_user)
+            return redirect('/')
     
     return render(request, 'signup.html')
 
