5e235576c9ce337cb8f749fc39eaf24a19bd81fc
alexi <aibasita@uci.edu>
2024-04-05 22:54:02 -0700

    Fixed Movies DB error with migration. Fixed default for movies with no description

--- flixquisite_site/core/models.py
+++ flixquisite_site/core/models.py
@@ -19,7 +19,7 @@ class Movie(models.Model):
         id = models.UUIDField(default=uuid.uuid4)
 
         title = models.CharField(max_length=255)
-        description = models.TextField()
+        description = models.TextField(default='No description available.')
         date = models.DateField()
         genre = models.CharField(max_length=50, choices=GENRES)
         length = models.PositiveBigIntegerField()
@@ -27,6 +27,32 @@ class Movie(models.Model):
         image_carousel_card = models.ImageField(upload_to='movie_images/')
         image_feature_cover = models.FileField(upload_to='movie_images/')
         playback = models.FileField(upload_to='movie_playback/')
+    # GENRES = [
+    #     ('action', 'Action'),
+    #     ('adventure', 'Adventure'),
+    #     ('horror', 'Horror'),
+    #     ('romance', 'Romance'),
+    #     ('science_fiction', 'Science Fiction'),
+    #     ('thriller', 'Thriller'),
+    #     ('fantasy', 'Fantasy'),
+    #     ('animation', 'Animation'),
+    #     ('documentary', 'Documentary'),
+    #     ('mystery', 'Mystery')
+    # ]
+
+    # uu_id = models.UUIDField(default=uuid.uuid4)
+
+    # title = models.CharField(max_length=255)
+    # description = models.TextField()
+    # release_date = models.DateField()
+    # genre = models.CharField(max_length=50, choices=GENRES)
+    # length = models.PositiveBigIntegerField()
+
+    # image_card = models.ImageField(upload_to='movie_images/')
+    # image_cover = models.FileField(upload_to='movie_images/')
+    # video = models.FileField(upload_to='movie_video/')
+    # movie_views = models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.title
