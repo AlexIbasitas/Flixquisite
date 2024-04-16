334a64b4a3065481cf345e29c485c2a0650d0b0f
alexi <aibasita@uci.edu>
2024-04-16 16:10:09 -0700

    Static files 2nd fix

--- netflix_pro_site/urls.py
+++ netflix_pro_site/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -26,3 +28,6 @@ urlpatterns = [
 
 # Added + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Added
+urlpatterns += staticfiles_urlpatterns()
