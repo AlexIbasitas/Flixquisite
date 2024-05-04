84750f978422f9f8a32109380710b5a180548721
alexi <aibasita@uci.edu>
2024-05-04 16:21:39 -0700

    reduced dataset to 1 year

--- core/management/commands/load_viewing_activity.py
+++ core/management/commands/load_viewing_activity.py
@@ -1,6 +1,62 @@
+# import csv
+# import datetime
+# from itertools import islice
+# from django.conf import settings
+# from django.core.management.base import BaseCommand
+# from core.models import NetflixMovie
+
+# class Command(BaseCommand):
+#     help = 'Load data from ViewingActivity file'
+
+#     def handle(self, *args, **kwargs):
+#         datafile = settings.BASE_DIR / 'data' / 'ViewingActivity.csv'
+
+#         one_year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
+
+#         with open(datafile, 'r', encoding='utf-8') as csvfile:
+#             reader = csv.DictReader(islice(csvfile, 0, 4952))
+#             # Profile Name,Start Time,Duration,Attributes,Title,Supplemental Video Type,Device Type,Bookmark,Latest Bookmark,Country
+#             for row in reader:
+#                 profile_name = row['Profile Name']
+#                 start_time = datetime.datetime.strptime(row['Start Time'], '%Y-%m-%d %H:%M:%S')
+
+#                 dur_hours, dur_minutes, dur_seconds = map(int, row['Duration'].split(':'))
+#                 duration = datetime.timedelta(hours=dur_hours, minutes=dur_minutes, seconds=dur_seconds)
+
+#                 attributes = row['Attributes']
+#                 title = row['Title']
+#                 supplemental_video_type = row['Supplemental Video Type']
+#                 device_type = row['Device Type']
+
+#                 book_hours, book_minutes, book_seconds = map(int, row['Bookmark'].split(':'))
+#                 bookmark = datetime.timedelta(hours=book_hours, minutes=book_minutes, seconds=book_seconds)
+
+#                 # Catch 'Not latest View' exception
+#                 latest_bookmark = datetime.timedelta(days=-1)
+#                 if row['Latest Bookmark'] != 'Not latest view':
+#                     latest_hours, latest_minutes, latest_seconds = map(int, row['Latest Bookmark'].split(':'))
+#                     latest_bookmark = datetime.timedelta(hours=latest_hours, minutes=latest_minutes, seconds=latest_seconds)
+
+#                 country = row['Country']
+
+#                 # Create and save NetflixMovie object
+#                 NetflixMovie.objects.create(
+#                     profile_name=profile_name,
+#                     start_time=start_time,
+#                     duration=duration,
+#                     attributes=attributes,
+#                     title=title,
+#                     supplemental_video_type=supplemental_video_type,
+#                     device_type=device_type,
+#                     bookmark=bookmark,
+#                     latest_bookmark=latest_bookmark,
+#                     country=country
+#                 )
+
+
+
 import csv
 import datetime
-from itertools import islice
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from core.models import NetflixMovie
@@ -11,42 +67,49 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         datafile = settings.BASE_DIR / 'data' / 'ViewingActivity.csv'
 
+        one_year_ago = datetime.datetime.now() - datetime.timedelta(days=365)
+        NetflixMovie.objects.filter(start_time__lt=one_year_ago).delete()
+
         with open(datafile, 'r', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(islice(csvfile, 0, 4952))
-            # Profile Name,Start Time,Duration,Attributes,Title,Supplemental Video Type,Device Type,Bookmark,Latest Bookmark,Country
+            reader = csv.DictReader(csvfile)
             for row in reader:
-                profile_name = row['Profile Name']
                 start_time = datetime.datetime.strptime(row['Start Time'], '%Y-%m-%d %H:%M:%S')
 
-                dur_hours, dur_minutes, dur_seconds = map(int, row['Duration'].split(':'))
-                duration = datetime.timedelta(hours=dur_hours, minutes=dur_minutes, seconds=dur_seconds)
+                # Check if the start time is within the last year
+                if start_time >= one_year_ago:
+                    profile_name = row['Profile Name']
 
-                attributes = row['Attributes']
-                title = row['Title']
-                supplemental_video_type = row['Supplemental Video Type']
-                device_type = row['Device Type']
+                    dur_hours, dur_minutes, dur_seconds = map(int, row['Duration'].split(':'))
+                    duration = datetime.timedelta(hours=dur_hours, minutes=dur_minutes, seconds=dur_seconds)
 
-                book_hours, book_minutes, book_seconds = map(int, row['Bookmark'].split(':'))
-                bookmark = datetime.timedelta(hours=book_hours, minutes=book_minutes, seconds=book_seconds)
+                    attributes = row['Attributes']
+                    title = row['Title']
+                    supplemental_video_type = row['Supplemental Video Type']
+                    device_type = row['Device Type']
 
-                # Catch 'Not latest View' exception
-                latest_bookmark = datetime.timedelta(days=-1)
-                if row['Latest Bookmark'] != 'Not latest view':
-                    latest_hours, latest_minutes, latest_seconds = map(int, row['Latest Bookmark'].split(':'))
-                    latest_bookmark = datetime.timedelta(hours=latest_hours, minutes=latest_minutes, seconds=latest_seconds)
+                    book_hours, book_minutes, book_seconds = map(int, row['Bookmark'].split(':'))
+                    bookmark = datetime.timedelta(hours=book_hours, minutes=book_minutes, seconds=book_seconds)
 
-                country = row['Country']
+                    # Catch 'Not latest View' exception
+                    latest_bookmark = datetime.timedelta(days=-1)
+                    if row['Latest Bookmark'] != 'Not latest view':
+                        latest_hours, latest_minutes, latest_seconds = map(int, row['Latest Bookmark'].split(':'))
+                        latest_bookmark = datetime.timedelta(hours=latest_hours, minutes=latest_minutes, seconds=latest_seconds)
 
-                # Create and save NetflixMovie object
-                NetflixMovie.objects.create(
-                    profile_name=profile_name,
-                    start_time=start_time,
-                    duration=duration,
-                    attributes=attributes,
-                    title=title,
-                    supplemental_video_type=supplemental_video_type,
-                    device_type=device_type,
-                    bookmark=bookmark,
-                    latest_bookmark=latest_bookmark,
-                    country=country
-                )
+                    country = row['Country']
+
+                    # Create and save NetflixMovie object
+                    NetflixMovie.objects.create(
+                        profile_name=profile_name,
+                        start_time=start_time,
+                        duration=duration,
+                        attributes=attributes,
+                        title=title,
+                        supplemental_video_type=supplemental_video_type,
+                        device_type=device_type,
+                        bookmark=bookmark,
+                        latest_bookmark=latest_bookmark,
+                        country=country
+                    )
+
+        self.stdout.write(self.style.SUCCESS('Data loaded successfully.'))
